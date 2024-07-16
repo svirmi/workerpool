046a3d15665063b9dc2dc542fe03d3007292b2c1
M.S <mikhail.svirkin@mail.ru>
2024-07-16 20:51:04 +0300

    single type of task

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	n := 20
+
+	tasks := make([]Task, n)
+
+	for i := 0; i < n; i++ {
+		tasks[i] = Task{ID: i + 1}
+	}
+
+	// Create a worker pool
+	wp := WorkerPool{
+		Tasks:       tasks,
+		concurrency: 5,
+	}
+
+	wp.Run()
+	fmt.Println("All tasks have been processed!")
 }
--- worker_pool.go
+++ worker_pool.go
@@ -1,9 +1,55 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
 // Task definition
+type Task struct {
+	ID int
+}
 
 // Way to process the tasks
+func (t *Task) Process() {
+	fmt.Printf("Processing task %d\n", t.ID)
+	// Simulate a time consuming process
+	time.Sleep(2 * time.Second)
+}
 
 // Worker pool definition
+type WorkerPool struct {
+	Tasks       []Task
+	concurrency int
+	tasksChan   chan Task
+	wg          sync.WaitGroup
+}
 
 // Functions to execute the worker pool
+func (wp *WorkerPool) worker() {
+	for task := range wp.tasksChan {
+		task.Process()
+		wp.wg.Done()
+	}
+}
+
+func (wp *WorkerPool) Run() {
+	n := len(wp.Tasks)
+	wp.tasksChan = make(chan Task, n)
+
+	// Start workers
+	for i := 0; i < wp.concurrency; i++ {
+		go wp.worker()
+	}
+
+	// Send tasks to the tasks channel
+	wp.wg.Add(n)
+	for _, task := range wp.Tasks {
+		wp.tasksChan <- task
+	}
+
+	close(wp.tasksChan)
+
+	wp.wg.Wait()
+}
